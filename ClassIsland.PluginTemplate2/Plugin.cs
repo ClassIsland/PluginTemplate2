66622465765b41765174f52583a8790c53bd041b
WRC <hello_wrc@outlook.com>
2025-12-13 22:11:53 +0800

    init

---
+++ ClassIsland.PluginTemplate2/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using ClassIsland.Core;
+using ClassIsland.Core.Abstractions;
+using ClassIsland.Core.Attributes;
+using ClassIsland.Core.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ClassIsland.PluginTemplate2;
+
+[PluginEntrance]
+public class Plugin : PluginBase
+{
+    public override void Initialize(HostBuilderContext context, IServiceCollection services)
+    {
+        AppBase.Current.AppStarted += async (_, _) => 
+            await CommonTaskDialogs.ShowDialog("Hello world!", "Hello from ClassIsland.PluginTemplate2!");
+    }
+}
